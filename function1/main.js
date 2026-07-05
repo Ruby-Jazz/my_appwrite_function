33d961b8af83b0a79f714cadc42bdead20a17333
Ruby-Jazz <reubenprecious214@gmail.com>
2026-07-05 16:15:36 +0100

    -

--- function1/main.js
+++ function1/main.js
@@ -1,16 +1,15 @@
-module.exports = async ({ req, res, log, error }) => {
-    switch (req.method) {
-        case "GET":
-            return res.text("join me as i create my first appwrite function");
-
-        case "POST":
+export default ({req,res,log,error})=>{
+    switch(req.method) {
+        case "GET": 
+            return res.text("join me as i create my first appwrite function")
+        
+        case "POST": 
             return res.json({
-                sentData: req.body,
-                RUBYENV: process.env.RUBYENV,
-                MIMAENV: process.env.MIMAENV
-            });
-
-        default:
-            return res.empty();
+                sentData : req.body, 
+                RUBYENV : process.env.RUBYENV,
+                MIMAENV:process.env.MIMAENV
+            })
+        
+        default : return res.empty();
     }
-};
+}
