ef126543fd97a9ed3eb30f506ebb2ef3551abf4e
Ruby-Jazz <reubenprecious214@gmail.com>
2026-07-05 16:13:13 +0100

    corrected res.status error

--- function1/main.js
+++ function1/main.js
@@ -1,15 +1,16 @@
-export default ({req,res,log,error})=>{
-    switch(req.method) {
-        case "GET": 
-            return res.status(200).text("join me as i create my first appwrite function")
-        
-        case "POST": 
-            return res.status(201).json({
-                sentData : req.body,
-                RUBYENV : process.env.RUBYENV,
-                MIMAENV:process.env.MIMAENV
-            })
-        
-        default : return res.empty();
+module.exports = async ({ req, res, log, error }) => {
+    switch (req.method) {
+        case "GET":
+            return res.text("join me as i create my first appwrite function");
+
+        case "POST":
+            return res.json({
+                sentData: req.body,
+                RUBYENV: process.env.RUBYENV,
+                MIMAENV: process.env.MIMAENV
+            });
+
+        default:
+            return res.empty();
     }
-}
+};
